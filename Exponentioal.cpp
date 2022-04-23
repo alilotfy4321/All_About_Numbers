817c87bf1e53451e36eb028569adf2c68dba429a
alilotfy4321 <96634396+alilotfy4321@users.noreply.github.com>
2022-04-23 22:01:20 +0200

    Add files via upload

---
+++ Exponentioal.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int powerr(int x,int n);
+int main(){
+	int x,n;
+	cin>>x>>n;
+	cout<<powerr(x,n);
+	
+	return 0;
+}
+
+
+int powerr(int x,int n){
+	int temp=1;
+	while(n){
+		if(n%2!=0){
+			temp=temp*x;
+		}
+		
+		n=n>>1;//n=n/2;
+		x=(x*x);
+	}
+	
+	return temp;
+}
